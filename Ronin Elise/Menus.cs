3534f47600adb37c121b382f3ff1e66acdf70ce4
Taazuma <tkaira@fyii.de>
2016-04-16 17:46:21 +0200

    Added files via upload

---
+++ Ronin Elise/Menus.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using Mario_s_Lib;
+
+namespace RoninElise
+{
+    internal class Menus
+    {
+        public const string ComboMenuID = "combomenuid";
+        public const string HarassMenuID = "harassmenuid";
+        //public const string AutoHarassMenuID = "autoharassmenuid";
+        public const string LaneClearMenuID = "laneclearmenuid";
+        //public const string LastHitMenuID = "lasthitmenuid";
+        public const string JungleClearMenuID = "jungleclearmenuid";
+        //public const string KillStealMenuID = "killstealmenuid";
+        public const string DrawingsMenuID = "drawingsmenuid";
+        public const string MiscMenuID = "miscmenuid";
+        public static Menu FirstMenu;
+        public static Menu ComboMenu;
+        public static Menu HarassMenu;
+        public static Menu AutoHarassMenu;
+        public static Menu LaneClearMenu;
+        public static Menu LasthitMenu;
+        public static Menu JungleClearMenu;
+        public static Menu KillStealMenu;
+        public static Menu DrawingsMenu;
+        public static Menu MiscMenu;
+
+        //These colorslider are from Mario`s Lib
+        public static ColorSlide QColorSlide;
+        public static ColorSlide WColorSlide;
+        public static ColorSlide EColorSlide;
+        public static ColorSlide RColorSlide;
+        public static ColorSlide DamageIndicatorColorSlide;
+
+        public static void CreateMenu()
+        {
+            FirstMenu = MainMenu.AddMenu("Ronin " + Player.Instance.ChampionName, Player.Instance.ChampionName.ToLower() + "hue");
+            ComboMenu = FirstMenu.AddSubMenu("• Combo", ComboMenuID);
+            HarassMenu = FirstMenu.AddSubMenu("• Harass", HarassMenuID);
+            //AutoHarassMenu = FirstMenu.AddSubMenu("• AutoHarass", AutoHarassMenuID);
+            LaneClearMenu = FirstMenu.AddSubMenu("• LaneClear", LaneClearMenuID);
+            //LasthitMenu = FirstMenu.AddSubMenu("• LastHit", LastHitMenuID);
+            JungleClearMenu = FirstMenu.AddSubMenu("• JungleClear", JungleClearMenuID);
+            //KillStealMenu = FirstMenu.AddSubMenu("• KillSteal", KillStealMenuID);
+            MiscMenu = FirstMenu.AddSubMenu("• Misc", MiscMenuID);
+            DrawingsMenu = FirstMenu.AddSubMenu("• Drawings", DrawingsMenuID);
+
+            ComboMenu.AddGroupLabel("Combo");
+            ComboMenu.AddLabel("ONLY CHECK ONE COMBO");
+            ComboMenu.AddLabel("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+            ComboMenu.CreateCheckBox("Combo Fights", "1Combo");
+            ComboMenu.AddSeparator(10);
+            ComboMenu.CreateCheckBox("Gank Combo", "2Combo");
+            ComboMenu.AddSeparator(10);
+            ComboMenu.AddLabel("Human Settings");
+            ComboMenu.CreateCheckBox(" - Use Q", "qUse");
+            ComboMenu.CreateCheckBox(" - Use W", "wUse");
+            ComboMenu.CreateCheckBox(" - Use E", "eUse");
+            ComboMenu.CreateCheckBox(" - Use R", "rUse");
+            ComboMenu.AddLabel("Spider Settings");
+            ComboMenu.CreateCheckBox(" - Use Q2", "q2Use");
+            ComboMenu.CreateCheckBox(" - Use W2", "w2Use");
+            ComboMenu.CreateCheckBox(" - Use E2", "e2Use");
+            ComboMenu.AddLabel("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+
+            HarassMenu.AddGroupLabel("Harass");
+            HarassMenu.AddLabel("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+            HarassMenu.AddLabel("Human Settings");
+            HarassMenu.CreateCheckBox(" - Use Q", "qUse");
+            HarassMenu.CreateCheckBox(" - Use W", "wUse", false);
+            HarassMenu.CreateCheckBox(" - Use E", "eUse");
+            HarassMenu.AddLabel("Spider Settings");
+            HarassMenu.CreateCheckBox(" - Use Q2", "q2Use");
+            HarassMenu.CreateCheckBox(" - Use W2", "w2Use", false);
+            HarassMenu.CreateCheckBox(" - Use E2", "e2Use", false);
+            HarassMenu.AddGroupLabel("Settings");
+            HarassMenu.CreateSlider("Mana must be higher than [{0}%] to use Harass spells", "manaSlider", 30);
+            HarassMenu.AddLabel("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+
+            LaneClearMenu.AddGroupLabel("LaneLasthitClear");
+            LaneClearMenu.AddLabel("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+            LaneClearMenu.CreateCheckBox(" - Use Q/Q2", "qUse");
+            LaneClearMenu.AddGroupLabel("Settings");
+            LaneClearMenu.CreateSlider("Mana must be higher than [{0}%] to use LaneClear spells", "manaSlider", 30);
+            LaneClearMenu.AddLabel("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+
+            JungleClearMenu.AddGroupLabel("JungleClear");
+            JungleClearMenu.AddLabel("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+            JungleClearMenu.AddLabel("Human Settings");
+            JungleClearMenu.CreateCheckBox(" - Use Q", "qUse");
+            JungleClearMenu.CreateCheckBox(" - Use W", "wUse");
+            JungleClearMenu.CreateCheckBox(" - Use E", "eUse");
+            JungleClearMenu.CreateCheckBox(" - Use R", "rUse");
+            JungleClearMenu.AddLabel("Spider Settings");
+            JungleClearMenu.CreateCheckBox(" - Use Q2", "q2Use");
+            JungleClearMenu.CreateCheckBox(" - Use W2", "w2Use");
+            JungleClearMenu.CreateCheckBox(" - Use E2", "e2Use");
+            JungleClearMenu.AddGroupLabel("Settings");
+            JungleClearMenu.CreateSlider("Mana must be higher than [{0}%] to use JungleClear spells", "manaSlider", 20);
+            JungleClearMenu.AddLabel("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+
+            //KillStealMenu.AddGroupLabel("KS");
+            //KillStealMenu.AddLabel("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+            //KillStealMenu.CreateCheckBox(" - Use Q", "qUse");
+            //KillStealMenu.CreateCheckBox(" - Use W", "wUse");
+            //KillStealMenu.CreateCheckBox(" - Use E", "eUse");
+            //KillStealMenu.CreateCheckBox(" - Use R", "rUse");
+            //KillStealMenu.AddGroupLabel("Settings");
+            //KillStealMenu.CreateSlider("Mana must be higher than [{0}%] to use Killsteal spells", "manaSlider", 30);
+            //KillStealMenu.AddLabel("▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
+
+            MiscMenu.CreateCheckBox("- Use Q Anti Gapcloser BETA", "eGap");
+            MiscMenu.AddGroupLabel("Skin Changer");
+            
+            var skinList = Mario_s_Lib.DataBases.Skins.SkinsDB.FirstOrDefault(list => list.Champ == Player.Instance.Hero);
+            if (skinList != null)
+            {
+                MiscMenu.CreateComboBox("Choose the skin", "skinComboBox", skinList.Skins);
+                MiscMenu.Get<ComboBox>("skinComboBox").OnValueChange += delegate(ValueBase<int> sender, ValueBase<int>.ValueChangeArgs args)
+                {
+                    Player.Instance.SetSkinId(sender.CurrentValue);
+                };
+            }
+
+            MiscMenu.AddGroupLabel("Auto Level UP");
+            MiscMenu.CreateCheckBox("Activate Auto Leveler", "activateAutoLVL", false);
+            MiscMenu.AddLabel("The auto leveler will always focus R than the rest of the spells");
+            MiscMenu.CreateComboBox("1st Spell to focus", "firstFocus", new List<string> {"Q", "W", "E"});
+            MiscMenu.CreateComboBox("2nd Spell to focus", "secondFocus", new List<string> {"Q", "W", "E"}, 1);
+            MiscMenu.CreateComboBox("3rd Spell to focus", "thirdFocus", new List<string> {"Q", "W", "E"}, 2);
+            MiscMenu.CreateSlider("Delay slider", "delaySlider", 200, 150, 500);
+
+            DrawingsMenu.AddGroupLabel("Setting");
+            DrawingsMenu.CreateCheckBox(" - Draw Spell`s range only if they are ready.", "readyDraw");
+            DrawingsMenu.CreateCheckBox(" - Draw damage indicator.", "damageDraw");
+            DrawingsMenu.CreateCheckBox(" - Draw damage indicator percent.", "perDraw");
+            DrawingsMenu.CreateCheckBox(" - Draw damage indicator statistics.", "statDraw", false);
+            DrawingsMenu.AddGroupLabel("Spells");
+            DrawingsMenu.CreateCheckBox(" - Draw Q.", "qDraw");
+            DrawingsMenu.CreateCheckBox(" - Draw W.", "wDraw");
+            DrawingsMenu.CreateCheckBox(" - Draw E.", "eDraw");
+            DrawingsMenu.CreateCheckBox(" - Draw R.", "rDraw");
+            DrawingsMenu.AddGroupLabel("Drawings Color");
+            QColorSlide = new ColorSlide(DrawingsMenu, "qColor", Color.Red, "Q Color:");
+            WColorSlide = new ColorSlide(DrawingsMenu, "wColor", Color.Purple, "W Color:");
+            EColorSlide = new ColorSlide(DrawingsMenu, "eColor", Color.Orange, "E Color:");
+            RColorSlide = new ColorSlide(DrawingsMenu, "rColor", Color.DeepPink, "R Color:");
+            DamageIndicatorColorSlide = new ColorSlide(DrawingsMenu, "healthColor", Color.YellowGreen, "DamageIndicator Color:");
+        }
+    }
+}
