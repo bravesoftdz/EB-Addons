0387a7616c48ba0be7283bcacaaf02328f0c34bf
Taazuma <tkaira@fyii.de>
2016-04-17 19:13:10 +0200

    Added files via upload

---
+++ Ronin Skarner/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using EloBuddy;
+using EloBuddy.SDK.Events;
+using RoninSkarner.Modes;
+
+namespace RoninSkarner
+{
+    internal class Program
+    {
+        // ReSharper disable once UnusedParameter.Local
+        /// <summary>
+        /// The firs thing that runs on the template
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+
+        /// <summary>
+        /// This event is triggered when the game loads
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            //Put the name of the champion here
+            if (Player.Instance.ChampionName != "Skarner") return;
+
+            SpellsManager.InitializeSpells();
+            Menus.CreateMenu();
+            ModeManager.InitializeModes();
+            DrawingsManager.InitializeDrawings();
+        }
+    }
+}
